1507be91d704caed5ce4db368d9ef7fb39db2fe5
sunnysan-jpg <03sunnyyadav@gmail.com>
2025-07-30 00:09:55 +0530

    url changed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ubiquitous-heliotrope-531f72.netlify.app');
+  res.header('Access-Control-Allow-Origin', 'https://ubiquitous-heliotrope-531f72.netlify.app','https://teal-chimera-cf3b26.netlify.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
