7b2a0f72260242ebe12c38418cb0ce520f226f6b
sunnysan-jpg <03sunnyyadav@gmail.com>
2025-07-31 23:02:38 +0530

    db changes added

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,27 @@
 const {  Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-  }
-)
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'postgres',
+//     logging: false,
+//   }
+// )
+
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Important for Neon SSL
+    },
+  },
+});
 
 module.exports = sequelize
