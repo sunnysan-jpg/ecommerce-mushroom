7263f79731c01434fd6fcefe114085d928518e86
sunnysan-jpg <03sunnyyadav@gmail.com>
2025-08-04 22:15:26 +0530

    payment

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { authMiddleware } = require('../middleware/auth.middleware')
-const createPayment = require('../controllers/payment.controller')
+const {createPayment, verifyPayment} = require('../controllers/payment.controller')
 const router = express.Router()
 
 
 router.post('/create',createPayment)
+router.post('/verify-payment',verifyPayment)
 
 
 module.exports = router
